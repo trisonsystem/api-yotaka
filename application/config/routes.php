30e943d0dca4f0b27ed3f505f9a1b6f96cae1564
pornpipat kantaran <pornpipatzaa@gmail.com>
2019-02-11 16:02:56 +0700

    edit route

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,11 +52,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] 	= 'StockController';
 $route['test'] 					= 'StockController/testapi';
 
-## member
-{
-    $route['infolang']              = 'LanguageController/infoLanguage';
-}
-##--
+$route['infolang']              = 'LanguageController/infoLanguage';
+
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
