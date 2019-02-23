d5c6a9e2cd13846b2ad458f41d146b593e07ee60
pornpipat kantaran <pornpipatzaa@gmail.com>
2019-02-23 12:08:08 +0700

    no message

---
+++ application/controllers/Mdivision/MdivisionController.php
@@ -0,0 +1,39 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Origin: *');
+
+class MdivisionController extends CI_Controller
+{
+    public $strUrl = "";
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->des_key  = $this->config->config['des_key'];
+        $this->load->model('MMdivision');
+        $this->load->model('MMaster');
+    }
+
+    public function Decode_TripleDES( $aData ){
+        $data = (isset($aData["data"])) ? $aData["data"] : $aData;
+        $dataReceive = TripleDES::decryptText($data, $this->des_key);
+        $dataReceive = json_decode($dataReceive, true);
+
+        if ($dataReceive == '') {
+            $arrRetrun = array("sflag" => false, "msg" => "Key TripleDES Error ");
+            echo json_encode($arrRetrun);
+            return;
+        }
+        return $dataReceive;
+    }
+   
+    public function search_divcode( $aData = "" ){
+        $aData    = $this->Decode_TripleDES( $_POST );
+        $res = $this->MMdivision->search_divcode( $aData );
+        print_r( json_encode($res) );
+        
+        // debug($arr_data);
+    }
+
+    
+}
